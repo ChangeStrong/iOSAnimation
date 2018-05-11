20228f62d36a8220502b58334b892550708e1f1e
ChangeStrong <491337430@qq.com>
2018-05-11 16:24:16 +0800

    添加动态遮罩动画

--- MaskAnimation/MaskAnimation/ViewController.m
+++ MaskAnimation/MaskAnimation/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "UIView+LLMask.h"
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIImageView *bgImageView;
 @end
 
 @implementation ViewController
@@ -29,6 +29,41 @@
     [self.view addSubview:view];
     [view addMaskPath:[view getBottomTrangelPathDegree:15] isReverse:NO];
     
+}
+- (IBAction)startAnimationAction:(UIButton *)sender {
+    [self startAnimation];
+}
+
+-(void)startAnimation
+{
+    //颜色渐变
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    gradientLayer.frame = self.bgImageView.bounds;
+    gradientLayer.startPoint = CGPointMake(0.5, 1);
+    gradientLayer.endPoint = CGPointMake(0.5, 0);
+    gradientLayer.colors = @[(__bridge id)[[UIColor orangeColor]colorWithAlphaComponent:0.2].CGColor,(__bridge id)[[UIColor orangeColor]colorWithAlphaComponent:1.0].CGColor];
+    [self.bgImageView.layer addSublayer:gradientLayer];
+    
+    //路径 作为遮罩
+    CAShapeLayer *shapelayer = [CAShapeLayer  layer];
+    shapelayer.position = CGPointMake(0, 0);
+    shapelayer.lineWidth = 120;
+    CGMutablePathRef path = CGPathCreateMutable();
+    
+    CGPathMoveToPoint(path, 0, self.bgImageView.frame.size.width/2.0, self.bgImageView.frame.size.height);
+    CGPathAddLineToPoint(path, 0, self.bgImageView.frame.size.width/2.0, 0);
+    shapelayer.path = path;
+    shapelayer.strokeColor = [UIColor greenColor].CGColor;//划线的颜色
+    shapelayer.fillColor = [UIColor clearColor].CGColor;
+    self.bgImageView.layer.mask = shapelayer;
+    
+    //对路径层添加动画
+    CABasicAnimation *animation = [CABasicAnimation animation];
+    //    animation.path = path;
+    animation.duration = 5.0;
+    animation.fromValue = [NSNumber numberWithFloat:0.0];
+    animation.toValue = [NSNumber numberWithFloat:1.0];
+    [shapelayer addAnimation:animation forKey:@"strokeEnd"];//对划线属性进行动画
 }
 
 
