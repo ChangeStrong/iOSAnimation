a78fd9eb9939f9d8d43d9ccb988f7acc72d60d7e
ChangeStrong <491337430@qq.com>
2018-05-31 17:57:30 +0800

    内部添加label

--- PieChart/PieChart/PieChartView.m
+++ PieChart/PieChart/PieChartView.m
@@ -68,19 +68,32 @@
         maskLayer.fillRule = kCAFillRuleNonZero;//kCAFillRuleEvenOdd画的区域 取反  解释为奇偶。
         [self.layer addSublayer:maskLayer];
         
-         CGFloat angle = obj.endAngel-(obj.endAngel-obj.startAngel)/2.0;
+         CGFloat angle = obj.endAngel-(obj.endAngel-obj.startAngel)/2.0;//扇形边缘中心位置的角度
+        //扇形边缘的中心点
+//        CGPoint aPoint = CGPointMake(_centerPoint.x+ _radius*cos(DEGREES_TO_RADIANS(angle)),_centerPoint.y+ _radius*sin(DEGREES_TO_RADIANS(angle)));
         
-        CGPoint aPoint = CGPointMake(_centerPoint.x+ _radius*cos(DEGREES_TO_RADIANS(angle)),_centerPoint.y+ _radius*sin(DEGREES_TO_RADIANS(angle)));
+        int width = 30;
+        //内部边缘文字
+        UILabel *label0 = [[UILabel alloc]init];
+        label0.adjustsFontSizeToFitWidth = YES;
+        label0.bounds = CGRectMake(0, 0, width, width);
+        CGFloat newRadius0 = (_radius-((width/2.0)/cos(DEGREES_TO_RADIANS(45))));//扇形的半径+label的对角线长度
+        //新半径扇形的边缘中心点
+        CGPoint bPoint0 = CGPointMake(_centerPoint.x+ newRadius0*cos(DEGREES_TO_RADIANS(angle)), _centerPoint.y+newRadius0*sin(DEGREES_TO_RADIANS(angle)));
+        label0.center = bPoint0;
+        label0.text = @"6";
+        label0.textAlignment = NSTextAlignmentCenter;
+        [self addSubview:label0];
+        
+        //外围边缘文字
         UILabel *label = [[UILabel alloc]init];
         label.text = @"%10";
-//        label.backgroundColor = [UIColor purpleColor];
-        int width = 30;
+        label.textAlignment = NSTextAlignmentCenter;
         label.adjustsFontSizeToFitWidth = YES;
         label.bounds = CGRectMake(0, 0, width, width);
-       
-        CGFloat newRadius = (_radius+((width/2.0)/cos(DEGREES_TO_RADIANS(45))));
+        CGFloat newRadius = (_radius+((width/2.0)/cos(DEGREES_TO_RADIANS(45))));//扇形的半径+label的对角线长度
+        //新半径扇形的边缘中心点
         CGPoint bPoint = CGPointMake(_centerPoint.x+ newRadius*cos(DEGREES_TO_RADIANS(angle)), _centerPoint.y+newRadius*sin(DEGREES_TO_RADIANS(angle)));
-        
         label.center = bPoint; //CGPointMake( aPoint.x+width/2.0, aPoint.y+((width/2.0)*tan(DEGREES_TO_RADIANS(angle))));
         [self addSubview:label];
     }];
